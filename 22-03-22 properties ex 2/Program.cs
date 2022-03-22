e3e63ef1b6dc38c649941355a43fde4e21d342e0
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-22 09:24:23 +0100

    Add project files.

---
+++ 22-03-22 properties ex 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _22_03_22_properties_ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage opslag = new Storage();
+            opslag.Teller = 2;
+            opslag.Noemer = 3;
+            Console.WriteLine(opslag.Teller);
+            Console.WriteLine(opslag.Noemer);
+            Console.WriteLine(opslag.Breuk);
+        }
+    }
+}
---
+++ 22-03-22 properties ex 2/Storage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22_03_22_properties_ex_2
+{
+    class Storage
+    {
+            
+        private int teller;
+        private int noemer;
+        private double breuk;
+
+        public int Teller
+        {
+            get { return teller; }
+            set { teller = value; }
+        }
+        public int Noemer
+        {
+            get { return noemer; }
+            set { noemer = value; }
+        }
+        public double Breuk
+        {
+            get { return breuk; }
+            set { breuk = value; }
+        }
+        public Storage()
+        {
+            teller = 20;
+            noemer = 10;
+        }
+    }
+}
